c67b36890cc332341d963fa9989c93334c18cc4e
IkkLivly <33955299+IkkLivly@users.noreply.github.com>
2017-12-02 16:28:27 -0800

    Add files via upload

---
+++ runtime.cs
@@ -0,0 +1,19 @@
+// A Hello World! program in C#.
+using System;
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World!");
+        
+        [SecurityCriticalAttribute]
+public int Run(
+	Window window
+)
+        
+        
+        }
+    }
+}
